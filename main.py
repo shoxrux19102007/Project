858ddd2ea55010036c9828ec8b650408238eb90c
shoxrux19102007 <dilshoda084@gmail.com>
2022-08-19 12:29:49 +0300

    My Project

---
+++ main.py
@@ -0,0 +1,5 @@
+n = int (input( ) )
+a = list (map(int, input().split( ) ) )
+b,c = map (int,input( ).split( ) )
+print (a.replecer(c,b))
+   
---
+++ new.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def index(request):
+    # Get an HttpRequest - the request parameter
+    # perform operations using information from the request.
+    # Return HttpResponse
+    return HttpResponse('Hello from Django!')
+
